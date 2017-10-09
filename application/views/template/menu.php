114306199a3ac91fa25f009d85d2758035b225ae
DESKTOP-96KRNN2\motta <benmotta@gmail.com>
2017-10-08 22:02:33 -0600

    Se cambio la forma del menu y los estilos, se quito la columna de la izquierda y se adiciono el enlace de reportes

--- application/views/template/menu.php
+++ application/views/template/menu.php
@@ -29,7 +29,7 @@
 		}
 ?>
 
-		<a class="navbar-brand" href="<?php echo $enlace; ?>">CEIV-CCV</a>
+		<a class="navbar-brand" href="#">CEIV-CCV</a>
 				
             </div>
             <!-- /.navbar-header -->
@@ -41,16 +41,41 @@
                 <!-- /.dropdown -->	
 				
 				
+
+				<li>
+					<a href="#"><i class="fa fa-user fa-fw"></i> <?php echo $this->session->firstname; ?></a>
+				</li>
+				
+				<li>
+					<a href="<?php echo base_url("encuesta/manzana"); ?>"><i class="fa fa-pencil-square-o fa-fw"></i> Formularios</a>
+				</li>
+
+				<li>
+					<a href="<?php echo base_url("reemplazo"); ?>"><i class="fa fa-retweet fa-fw"></i> Reemplazos</a>
+				</li>
+				
+
 				
 				
+<?php 
+if($userRol==1){ //SI es usuario ADMIN o SUPERVISOR
+?>				
+		<li class="dropdown">
+			<a class="dropdown-toggle" data-toggle="dropdown" href="#">
+				<i class="fa fa-list-alt"></i> Reportes <i class="fa fa-caret-down"></i>
+			</a>
+			<ul class="dropdown-menu dropdown-messages">
+			
+				<li>
+					<a href="<?php echo base_url("public/reportico/run.php?execute_mode=MENU&project=ICFES"); ?>"><i class="fa fa-search fa-fw"></i> Ver formularios</a>
+				</li>
 				
-				
-				
-				
-				
-				
-				
-				
+			</ul>
+		</li>
+<?php 
+}
+?>
+			
 				
 				
 				
@@ -92,26 +117,4 @@ if($userRol==1){ //ADMIN
 			
 
 
-            <div class="navbar-default sidebar" role="navigation">
-                <div class="sidebar-nav navbar-collapse">
-                    <ul class="nav" id="side-menu">
-
-                        <li>
-                            <a href="#"><i class="fa fa-user fa-fw"></i> <?php echo $this->session->firstname; ?></a>
-                        </li>
-						
-                        <li>
-							<a href="<?php echo base_url("encuesta/manzana"); ?>"><i class="fa fa-pencil-square-o fa-fw"></i> Formularios</a>
-                        </li>
-
-                        <li>
-							<a href="<?php echo base_url("reemplazo"); ?>"><i class="fa fa-retweet fa-fw"></i> Reemplazos</a>
-                        </li>
-						
-
-                    </ul>
-                </div>
-                <!-- /.sidebar-collapse -->
-            </div>
-            <!-- /.navbar-static-side -->
         </nav>
